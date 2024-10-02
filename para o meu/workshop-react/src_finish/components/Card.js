96877d21d0a7e0b8dfb37407d932603b3fda9d2d
daniel <162482141+danielcbz@users.noreply.github.com>
2024-10-02 17:19:32 -0300

    Add files via upload

---
+++ para o meu/workshop-react/src_finish/components/Card.js
@@ -0,0 +1,37 @@
+import style from "./Card.module.css";
+
+function Card({ banner, title, date }) {
+  const [show, setShow] = useState(false);
+  const handleClick = () => {
+    setShow(!show);
+  };
+
+  return (
+    <div className={style.card}>
+      <figure>
+        <img src={banner} />
+      </figure>
+      <article>
+        <h1>{title}</h1>
+        <div>
+          <time>{date}</time>
+          {show ? (
+            <img className={style.icon} src="icons/heart-solid.svg" onClick={handleClick} />
+          ) : (
+            <img className={style.icon} src="icons/heart-regular.svg" onClick={handleClick} />
+          )}
+        </div>
+      </article>
+    </div>
+  );
+}
+
+export default Card;
+
+{
+  show ? (
+    <img className={style.icon} src="icons/heart-solid.svg" onClick={handleClick} />
+  ) : (
+    <img className={style.icon} src="icons/heart-regular.svg" onClick={handleClick} />
+  );
+}
